c58ea5bd79d7a0c9c667c3ac97dfedaedef92c84
wincorex64a <minecraft.villagerssite@mail.ru>
2024-12-21 13:43:11 +0000

    Upgraded stylesheet, added Gotham font, added initial logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import './styles.css';
+import "./styles.css";
 
 export default function App() {
-	return <>
-		<header>
-			<img id="icon"></img>
-			<a id="news"></a>
-		</header>
-		<main>
-			<div id="container">
-				
-			</div>
-			<div id="upcoming">
-				
-			</div>
-		</main>
-	</>;
+	return (
+		<>
+			<header>
+				<img height="80" src="logo.png" id="icon"></img>
+				<a id="news"></a>
+			</header>
+			<main>
+				<div className="frame" id="container"></div>
+				<div className="frame" id="upcoming"></div>
+			</main>
+		</>
+	);
 }
