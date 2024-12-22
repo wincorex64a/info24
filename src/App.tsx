7fc018610ccba85f8f4e3b14890179b87b8d5923
wincorex64a <minecraft.villagerssite@mail.ru>
2024-12-22 15:38:41 +0000

    Automatic commit before Assistant edits

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 
 export default function App() {
 	return <>
+		
 		<header>
 			<a href="https://litsey24.ru/index" id="icon-container">
 				<img height="80" src="logo.png" id="icon"></img>
