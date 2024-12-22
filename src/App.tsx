227e25341c88f0764d9a30aeab11a355727e6af4
wincorex64a <minecraft.villagerssite@mail.ru>
2024-12-22 11:40:12 +0000

    Automatic commit before Assistant edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
 import "./styles.css";
 
 export default function App() {
-	return (
-		<>
-			<header>
-				<a href="https://litsey24.ru/index" id="icon-container">
-					<img height="80" src="logo.png" id="icon"></img>
+	return <>
+		<header>
+			<a href="https://litsey24.ru/index" id="icon-container">
+				<img height="80" src="logo.png" id="icon"></img>
+			</a>
+			<a id="news">Новости</a>
+			<div id="datetime">
+				<a id="date">
+					<a id="day">20</a>
+					<span className="dot">.</span>
+					<a id="month">08</a>
+					<span className="dot">.</span>
+					<a id="year">1993</a>
 				</a>
-				<a id="news">Новости</a>
-			</header>
-			<main>
-				<div className="frame" id="container"></div>
-				<div className="frame" id="upcoming"></div>
-			</main>
-		</>
-	);
+				<a id="time">
+					<a id="hour">12</a>
+					<span className="colon">:</span>
+					<a id="minute">00</a>
+				</a>
+			</div>
+		</header>
+		<main>
+			<div className="frame" id="class-select">
+				<select id="class-selector">
+					<option value="5А">5А</option>
+					<option value="5Б">5Б</option>
+					<option value="6А">6А</option>
+					<option value="6Б">6Б</option>
+					<option value="7А">7А</option>
+					<option value="7Б">7Б</option>
+					<option value="8А">8А</option>
+					<option value="8Б">8Б</option>
+					<option value="9А">9А</option>
+					<option value="9Б">9Б</option>
+					<option value="10А">10А</option>
+					<option value="10Б">10Б</option>
+					<option value="11А">11А</option>
+				</select>
+			</div>
+			<div className="frame" id="schedule"></div>
+		</main>
+	</>
 }
