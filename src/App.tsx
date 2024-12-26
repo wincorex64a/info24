99960b7830a7d061bd2b43b4c93908b44c85a0c4
wincorex64a <minecraft.villagerssite@mail.ru>
2024-12-26 11:57:02 +0000

    Automatic commit before Assistant edits

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ export function loaderButtonEvent(): void {
 			break;
 	}
 	if (dotw == "sunday") return;
-	const arr = data.classes[selectedClass][dotw];
+	const arr = data[selectedClass][dotw];
 	for (var i = 1; i < arr.length; i++) {
 		schedule.appendChild(createScheduleElement(i, arr.name, arr.classroom, arr.was == null));
 	}
@@ -99,52 +99,4 @@ window.onload = () => {
 	if (loadButton) {
 		loadButton.onclick = loaderButtonEvent;
 	}
-}
-
-
-
-export default function App() {
-	return <>
-		<header>
-			<a href="https://litsey24.ru/index" id="icon-container">
-				<img height="80" src="logo.png" id="icon"></img>
-			</a>
-			<a id="news">Новости</a>
-			<div id="datetime">
-				<div id="date">
-					<a id="day">20</a>
-					<span className="dot">.</span>
-					<a id="month">08</a>
-					<span className="dot">.</span>
-					<a id="year">1993</a>
-				</div>
-				<div id="time">
-					<a id="hour">12</a>
-					<span className="colon">:</span>
-					<a id="minute">00</a>
-				</div>
-			</div>
-		</header>
-		<main>
-			<div className="frame" id="class-select">
-				<select id="class-selector">
-					<option value="_5A">5А</option>
-					<option value="_5B">5Б</option>
-					<option value="_6A">6А</option>
-					<option value="_6B">6Б</option>
-					<option value="_7A">7А</option>
-					<option value="_7B">7Б</option>
-					<option value="_8A">8А</option>
-					<option value="_8B">8Б</option>
-					<option value="_9A">9А</option>
-					<option value="_9B">9Б</option>
-					<option value="_10A">10А</option>
-					<option value="_10B">10Б</option>
-					<option value="_11A">11</option>
-				</select>
-				<input type="button" value="Загрузить" onClick={loaderButtonEvent} />
-			</div>
-			<div className="frame" id="schedule"></div>
-		</main>
-	</>
 }
